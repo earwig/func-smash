62a9e9c2c383c948d175531326bf68f32618295d
Ben Kurtovic <ben.kurtovic@verizon.net>
2012-04-20 20:49:13 -0400

    Adding maths corpus

---
+++ corpora/maths.py
@@ -0,0 +1,25 @@
+def f1(a):
+    b = a + 9.0
+    return b
+
+def f2(a):
+    b = a - 7.0
+    return b
+
+def f3(a):
+    b = a * 5.0
+    return b
+
+def f4(a):
+    b = a / 3.0
+    return b
+
+def f5(a):
+    b = a ** 2.0
+    return b
+
+def f6(a):
+    b = a % 10.0
+    return b
+
+corpus = [f1, f2, f3, f4, f5, f6]
